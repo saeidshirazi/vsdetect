9c6163c60cdbcafc51b13a87472e67400dfcfec4
UnderCover <und3rcover96@gmail.com>
2018-10-24 23:55:04 +0330

    test

--- vsdetect.py
+++ vsdetect.py
@@ -3,4 +3,5 @@ url = 'https://www.virustotal.com/vtapi/v2/file/scan'
 params = {'apikey': '37b5d1312dcee2d2b31f198233162d098248bb35eed2b4b3cf40aae673ba2344'}
 files = {'file': ('myfile.exe', open('myfile.exe', 'rb'))}
 response = requests.post(url, files=files, params=params)
+
 print(response.json())
