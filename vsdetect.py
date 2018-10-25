f5d6badebfc660d1dd819fe3a6671a42202492f1
UnderCover <und3rcover96@gmail.com>
2018-10-25 22:37:18 +0330

    beta version

--- vsdetect.py
+++ vsdetect.py
@@ -1,7 +1,19 @@
 import requests
+from urllib.request import urlopen
+from bs4 import BeautifulSoup
+import webbrowser
+
 url = 'https://www.virustotal.com/vtapi/v2/file/scan'
 params = {'apikey': '37b5d1312dcee2d2b31f198233162d098248bb35eed2b4b3cf40aae673ba2344'}
 files = {'file': ('myfile.exe', open('myfile.exe', 'rb'))}
 response = requests.post(url, files=files, params=params)
+res=response.json()
+linkvs=res['permalink']
+webbrowser.open_new(str(linkvs))
 
-print(response.json())
+# bs = BeautifulSoup(html, 'lxml')
+# print(bs)
+# for l in  bs.findAll('td'):
+#     if l.find('Detection ratio:'):
+#         l.find('Detection ratio:').extract()
+#     print(l.getText())
